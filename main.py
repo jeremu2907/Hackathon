7a6c6a768660a536dfe7148ab32fa9663b7bbc3d
jeremu2907 <jeremu2907@gmail.com>
2022-11-12 17:22:32 -0600

    adder function

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import pymongo
-from pymongo import MongoClient
 
-cluster = MongoClient("mongodb+srv://username:2907@cluster0.h4uost6.mongodb.net/?retryWrites=true&w=majority")
+cluster = pymongo.MongoClient("mongodb+srv://username:2907@cluster0.h4uost6.mongodb.net/?retryWrites=true&w=majority")
 db = cluster["test"]
 usersCol = db["users"]
 eventsCol = db["events"]
-
-def addUser(data):
-    usersCol.insert_one(data)
 
 def addEvent(data):
     eventsCol.insert_one(data)
@@ -15,6 +11,16 @@ def addEvent(data):
 def delEvent(name):
     eventsCol.delete_one({"name" : name})
 
+def addUser(data):
+    usersCol.insert_one(data)
+
 def delUser(id):
     usersCol.delete_one({"id": id})
 
+def sortEvents(skillList):
+    things = eventsCol.find({"skills" : {"$in": skillList}})
+    for stuff in things:
+        print(stuff)
+    return things
+
+sortEvents(["animals", "finance"])

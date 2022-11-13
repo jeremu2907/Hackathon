a5b7ad78c493361994f5e5e4f0b08eb92d6fb6ee
chloepasc <chloepasc@yahoo.com>
2022-11-13 01:13:48 -0600

    Chloe's branch

---
+++ index.js
@@ -0,0 +1,27 @@
+// fucntion to search by location send long and lat < parameters
+
+function formatEvent(){
+    // var newListing = document.createElement("div");
+
+    // newListing.innerHTML = 
+
+    // var eventPosition = document.getElementByClass.createElement("listing-position");
+
+
+    // // const eventLocation = document.getElementByClass.createElement("listing-location");
+    // // const eventHost = document.getElementByClass.createElement("listing-host");
+    // // const eventTime = document.getElementByClass.createElement("listing-time");
+    // // const eventDate = document.getElementByClass.createElement("listing-date");
+    // // const eventSkills = document.getElementByClass.createElement("listing-skills");
+    // // const eventDescription = document.getElementByClass.createElement("listing-description");
+
+    // const allListings = document.getElementById("the-listings");
+    // allListings.appendChild(para);
+
+
+
+
+
+
+
+}
